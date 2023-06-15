5a6a7a9db46d64a7241805966047d911ca14c3c0
Ildar-Ilyasov <ildar.ilyasov123@yandex.com>
2023-06-15 13:57:54 +0300

    Initial commit

---
+++ src/com/company/Charger.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public interface Charger {
+    void charger(PhoneCharger charger);
+}
---
+++ src/com/company/Voltage.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Voltage {
+
+    public void connect(){
+        System.out.println("Устройство подключено к напряжению 380V");
+    }
+}
